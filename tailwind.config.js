5f71e3b5d17422f6da8e4c2ecae1b739c3e12b0f
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-10-13 04:20:21 +0500

    Ready, Set, Go!

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        sf: "var(--font-sfpro)"
+      }
     },
   },
   plugins: [],
